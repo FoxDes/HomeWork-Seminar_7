b8f88135dcf82206fe190d5dd2d8231b24e0de29
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-29 19:48:22 +0500

    Выполнил задачу 47 по дз

--- Program.cs
+++ Program.cs
@@ -3,10 +3,42 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-// Console.Clear();
 
+Console.Clear();
 
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
 
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
 
@@ -72,39 +104,39 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-Console.Clear();
-Console.Write("Введите количество строк: ");
-int rows = int.Parse(Console.ReadLine());
-Console.Write("Введите количество столбцов: ");
-int columns = int.Parse(Console.ReadLine());
+// Console.Clear();
+// Console.Write("Введите количество строк: ");
+// int rows = int.Parse(Console.ReadLine());
+// Console.Write("Введите количество столбцов: ");
+// int columns = int.Parse(Console.ReadLine());
 
-int [,] matrix = new int[rows, columns];
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(0); j++)
-    {
-        matrix[i,j] = new Random().Next(0, 10);
-    }
-}
+// int [,] matrix = new int[rows, columns];
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(0); j++)
+//     {
+//         matrix[i,j] = new Random().Next(0, 10);
+//     }
+// }
 
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-    for (int j = 0; j < matrix.GetLength(0); j++)
-    {
-        Console.Write($"{matrix[i,j]} ");
-    }
-    Console.WriteLine();
-}
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(0); j++)
+//     {
+//         Console.Write($"{matrix[i,j]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-Console.Write($"Cреднее арифметическое столбцов: ");
-Console.WriteLine();
-for (int i = 0; i < columns; i++)
-{
-  double arithmeticMean = 0;
-  for (int j = 0; j < rows; j++)
-  {
-    arithmeticMean += matrix[j, i];
-  }
-  arithmeticMean = Math.Round(arithmeticMean / rows, 1);
-  Console.WriteLine($" № {i+1} - {arithmeticMean}");
-}
+// Console.Write($"Cреднее арифметическое столбцов: ");
+// Console.WriteLine();
+// for (int i = 0; i < columns; i++)
+// {
+//   double arithmeticMean = 0;
+//   for (int j = 0; j < rows; j++)
+//   {
+//     arithmeticMean += matrix[j, i];
+//   }
+//   arithmeticMean = Math.Round(arithmeticMean / rows, 1);
+//   Console.WriteLine($" № {i+1} - {arithmeticMean}");
+// }
