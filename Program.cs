50332b85e9791d7dfa1508403ab882d3a1dd45dc
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-30 00:20:18 +0500

    выполнил 50 задачу по дз

--- Program.cs
+++ Program.cs
@@ -4,76 +4,31 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
-Console.Clear();
-
-Console.Write("Введите количество строк массива: ");
-int rows = int.Parse(Console.ReadLine());
-
-Console.Write("Введите количество столбцов массива: ");
-int columns = int.Parse(Console.ReadLine());
-
-double[,] array = GetArray(rows, columns, -10, 10);
-PrintArray(array);
-
-double[,] GetArray(int m, int n, double minValue, double maxValue)
-{
-    double[,] result = new double[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            result[i, j] = new Random().NextDouble() * 20 - 10;
-        }
-    }
-    return result;
-}
-
-void PrintArray(double[,] inArray)
-{
-    for (int i = 0; i < inArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < inArray.GetLength(1); j++)
-        {
-            Console.Write($"{inArray[i,j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-
-
-// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-// и возвращает значение этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 17 -> такого числа в массиве нет
-
 // Console.Clear();
-// Console.WriteLine("Введите номер строки: ");
-// int str = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите номер столбца: ");
-// int row = Convert.ToInt32(Console.ReadLine());
 
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
 
-// int[,] array = GetArray(6, 6, 0, 10);
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// double[,] array = GetArray(rows, columns, -10, 10);
 // PrintArray(array);
 
-// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// double[,] GetArray(int m, int n, double minValue, double maxValue)
 // {
-//     int[,] result = new int[m, n];
+//     double[,] result = new double[m, n];
 //     for (int i = 0; i < m; i++)
 //     {
 //         for (int j = 0; j < n; j++)
 //         {
-//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//             result[i, j] = new Random().NextDouble() * 20 - 10;
 //         }
 //     }
 //     return result;
 // }
 
-// void PrintArray(int[,] inArray)
+// void PrintArray(double[,] inArray)
 // {
 //     for (int i = 0; i < inArray.GetLength(0); i++)
 //     {
@@ -85,16 +40,59 @@ void PrintArray(double[,] inArray)
 //     }
 // }
 
-// void WantedArray(int[,] inArray)
-// {
-//     Console.Write($"Искомое число - " + inArray[str, row]);
-//     for (int i = 0; i != str; i++)
-//     {
-//         for (int j = 0; j != row; j++);
-//     }
-// }
 
-// WantedArray(array);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+Console.WriteLine("Введите номер строки: ");
+int str = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(6, 6, 0, 10);
+PrintArray(array);
+ErrorArray(array, row, str);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) 
+{ 
+ int[,] result = new int[m, n]; 
+ for (int i = 0; i < m; i++) 
+ { 
+ for (int j = 0; j < n; j++) 
+ { 
+ result[i, j] = new Random().Next(minValue, maxValue + 1); 
+ int temp = i +j; 
+ result[i, j] = i + j; 
+ } 
+ } 
+ return result; 
+} 
+void PrintArray(int[,] inArray) 
+{ 
+ for (int i = 0; i < inArray.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < inArray.GetLength(1); j++) 
+            { Console.Write($"{inArray[i, j]} "); } 
+ Console.WriteLine(); 
+    } 
+}
+
+void ErrorArray(int[,] inArray, int rowPos, int columnPos)
+{
+    if (row > inArray.GetLength(0) || str > inArray.GetLength(1))
+    {
+        Console.WriteLine("Числа с таким индексом в массиве нет!");
+    }
+}
+
+Console.WriteLine($"Искомое число - " + array[str, row]);
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
